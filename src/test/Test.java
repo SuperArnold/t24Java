050bb7a2768697362cbba10ecefe91aa29648d0a
SuperArnold <ji394vul35520@hotmail.com>
2022-03-23 14:04:38 +0800

    init

---
+++ src/test/Test.java
@@ -0,0 +1,19 @@
+package test;
+
+/**
+ * TODO: Document me!
+ *
+ * @author yuting
+ *
+ */
+public class Test {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
